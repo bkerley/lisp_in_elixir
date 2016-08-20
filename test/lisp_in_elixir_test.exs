acf2dc20971ce223909d4f2dd45f878fe3177ddd
Bryce Kerley <bkerley@brycekerley.net>
2016-08-20 15:05:16 -0400

    test for map circle-area and fibonacci

--- test/lisp_in_elixir_test.exs
+++ test/lisp_in_elixir_test.exs
@@ -84,7 +84,11 @@ defmodule LispInElixirTest do
     assert {pi_nine, ^with_circle_env} =
       eval("(circle-area 3)", with_circle_env)
 
-    assert_in_delta pi_nine, 28.27433, @delta
+    assert_in_delta pi_nine, 28.27431, @delta
+
+    assert {[3.14159, 28.27431], ^with_circle_env} = eval("""
+    (map circle-area (quote (1 3)))
+    """, with_circle_env)
   end
 
   test "factorial" do
@@ -105,6 +109,17 @@ defmodule LispInElixirTest do
             (count item (cdr L)))
             0)))
       (count (quote the) (quote (the more the merrier the bigger the better))))
+    """)
+  end
+
+  test "fibonacci" do
+    assert [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233,
+            377, 610, 987, 1597, 2584, 4181, 6765] == eval("""
+    (begin
+      (define fib (lambda (n) (if (< n 2) 1 (+ (fib (- n 1)) (fib (- n 2))))))
+      (define range (lambda (a b) (if (= a b) (quote ()) (cons a (range (+ a 1) b)))))
+      (map fib (range 0 20))
+      )
     """)
   end
 end
